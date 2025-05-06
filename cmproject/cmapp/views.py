26c8a3731e441280608e312f7211b938273c63ca
geektechdude <34283325+geektechdude@users.noreply.github.com>
2025-05-06 21:37:07 +0100

    added view for task_list

--- cmproject/cmapp/views.py
+++ cmproject/cmapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from cmapp.models import Task
+from cmapp.serializers import TaskSerializer
+
+
+@api_view(['GET', 'POST'])
+def task_list(request):
+    '''
+    '''
+    if request.method == 'GET':
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
